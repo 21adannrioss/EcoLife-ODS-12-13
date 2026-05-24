d2b9fd1a98fbf14ad13a30cedb62370168b3083d
21adannrioss <adan.elouadah.7e9@itb.cat>
2026-05-25 00:14:00 +0200

    fix: rutas corregidas.

--- public/includes/nav.php
+++ public/includes/nav.php
@@ -1,16 +1,16 @@
 <nav>
-    <a href="../index.html" class="logo">EcoLife</a>
+    <a href="../index.php" class="logo">EcoLife</a>
     <ul>
-        <li><a href="../index.html">Inici</a></li>
-        <li><a href="../view/ods.html">ODS</a></li>
-        <li><a href="../view/reptes.html">Reptes</a></li>
-        <li><a href="../view/habits.html">Hàbits</a></li>
-        <li><a href="../view/mercat.html">Mercat</a></li>
-        <li><a href="../view/calculadora.html">Calculadora</a></li>
-        <li><a href="../view/sostenibilitat.html" class="actiu">Sostenibilitat</a></li>
-        <li><a href="../view/empresa.html">Empresa</a></li>
-        <li><a href="../view/estadistiques.html">Estadístiques</a></li>
-        <li><a href="../view/sobre.html">Sobre</a></li>
+        <li><a href="../index.php">Inici</a></li>
+        <li><a href="../view/ods.php">ODS</a></li>
+        <li><a href="../view/reptes.php">Reptes</a></li>
+        <li><a href="../view/habits.php">Hàbits</a></li>
+        <li><a href="../view/mercat.php">Mercat</a></li>
+        <li><a href="../view/calculadora.php">Calculadora</a></li>
+        <li><a href="../view/sostenibilitat.php" class="actiu">Sostenibilitat</a></li>
+        <li><a href="../view/empresa.php">Empresa</a></li>
+        <li><a href="../view/estadistiques.php">Estadístiques</a></li>
+        <li><a href="../view/sobre.php">Sobre</a></li>
     </ul>
-    <button id="btn-dark" class="boto-dark" title="Canviar mode">Fosc</button>
+    <button id="btn-dark" class="boto-dark" title="Canviar mode">🌙</button>
 </nav>
